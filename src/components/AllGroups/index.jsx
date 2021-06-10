c76771cb7508bd47ccce58d5f97531d0fcec63c5
leorjr <l.grodriguesjr@gmail.com>
2021-06-10 15:27:54 -0300

    feat: buttons getting previous and next page

--- src/components/AllGroups/index.jsx
+++ src/components/AllGroups/index.jsx
@@ -1,29 +1,64 @@
 import GroupCard from "../GroupCard";
 
+import { ContainerGroups, ContainerStyled } from "./style.js";
+
 import api from "../../services/api";
 import { useState } from "react";
 import { useEffect } from "react";
 
 const AllGroups = () => {
   const [groups, setGroups] = useState([]);
+  const [url] = useState("/groups/");
+  const [next, setNext] = useState("");
+  const [previous, setPrevious] = useState("");
 
-  useEffect(() => {
+  const getGroups = (url) => {
     api
-      .get("/groups/")
+      .get(url)
       .then((res) => {
         setGroups(res.data.results);
+        setNext(res.data.next);
+        setPrevious(res.data.previous);
       })
       .catch((err) => console.log(err));
-  }, [groups]);
+  };
+
+  useEffect(() => {
+    getGroups(url);
+  }, []);
+
+  const handleNext = () => {
+    getGroups(next);
+  };
+
+  const handlePrev = () => {
+    if (previous !== null) {
+      getGroups(previous);
+    }
+  };
 
   return (
-    <div>
-      <div>
+    <ContainerStyled>
+      {previous !== null ? (
+        <button onClick={handlePrev}>Voltar</button>
+      ) : (
+        <button onClick={handlePrev} disabled>
+          Voltar
+        </button>
+      )}
+      <ContainerGroups>
         {groups.map((group) => {
           return <GroupCard key={group.id} name={group.name} />;
         })}
-      </div>
-    </div>
+      </ContainerGroups>
+      {next !== null ? (
+        <button onClick={handleNext}>Avançar</button>
+      ) : (
+        <button onClick={handleNext} disabled>
+          Avançar
+        </button>
+      )}
+    </ContainerStyled>
   );
 };
 
--- src/components/AllGroups/style.js
+++ src/components/AllGroups/style.js
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+
+export const ContainerStyled = styled.div`
+  width: 100%;
+  background-color: white;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ContainerGroups = styled.div`
+  width: 80%;
+
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`;
